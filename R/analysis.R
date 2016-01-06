74f408d60d33daaf51780e0050798a6ef7a10a07
George Wang <biogeek@gmail.com>
2016-01-06 01:46:27 -0800

    Finish time series generator example.

--- R/analysis.R
+++ R/analysis.R
@@ -20,9 +20,35 @@
 #'   this are negative.
 #' @export
 #' @examples
-#' gen_cycling_rec(years = 3,
-#'                 spectrum = data.frame(frequency = c(1, 1 / 365)
-#'                                       cyc_range = c(0.25, )))
+#' library(ggplot2)
+#' tropical_spectrum <- data.frame(frequency = c(1, 1 / 365),
+#'                                 cyc_range = c(6.53, 4.1),
+#'                                 phase     = c(0.421,0.189),
+#'                                 tau       = c(0,0))
+#' tropical_mean <- 25.845
+#' n_temperate_spectrum <- data.frame(frequency = c(1, 1 / 365),
+#'                                    cyc_range = c(5.33, 26.36),
+#'                                    phase     = c(0.319,1.057),
+#'                                    tau       = c(0,0))
+#' n_temperate_mean <- 10.457
+#' tropical_ts <- gen_cycling_rec(years    = 2,
+#'                                spectrum = tropical_spectrum,
+#'                                mean     = tropical_mean)
+#' tropical_ts$region <- "Tropical"
+#' n_temperate_ts <- gen_cycling_rec(years    = 2,
+#'                                   spectrum = n_temperate_spectrum,
+#'                                   mean     = n_temperate_mean)
+#' n_temperate_ts$region <- "North Temperate"
+#' ts_data <- rbind(tropical_ts,
+#'                  n_temperate_ts)
+#' ts_data <- factor(ts_data$region,
+#'                   levels = c("Tropical",
+#'                              "North Temperate"))
+#' ggplot(data = ts_data,
+#'        aes(x = jday,
+#'            y = temperature,
+#'            color = region)) +
+#'    geom_line(alpha = 0.7)
 gen_cycling_rec <- function(timebase   = NULL,
                             years      = NULL,
   spectrum = stop("data frame of data.frame(frequency, cyc_range, phase, tau) required"),
